4078c70aeaf9df6579fde630525307d1db0de3d7
Andy Strube <andystrube64@gmail.com>
2022-02-07 13:08:42 -0800

    Added styling for issues

--- app/javascript/components/Issue.jsx
+++ app/javascript/components/Issue.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function Issue(props) {
-  return <a className="green" href={`${props.root_url}/issues/${props.issue.id}`}>
-    {props.issue.title}
-  </a>
+  return <div className="issue-box">
+    <h5>{props.issue.title}</h5>
+    <a className="green" href={`${props.root_url}/issues/${props.issue.id}`}>
+      See Details
+    </a>
+  </div>
 }
 
 export default Issue;
